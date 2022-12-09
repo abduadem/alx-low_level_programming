8f677c7baf08ade2af457fee9ad1463c973ba1e5
abduadem <abdulrehmanadem4@gmail.com>
2022-12-09 12:45:29 -0800

    Task 11. The success combination in business is: Do what you do better...and: do more of what you d

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of three digits
+ * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
