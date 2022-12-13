bae385a1f63f8758f0d03eed53731279d090b75c
abduadem <abdulrehmanadem4@gmail.com>
2022-12-13 06:53:07 -0800

    Task 2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,18 +9,17 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
+	i = 0;
 
-	i = 0
-
-		while (i < 10)
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
+	}
 }
