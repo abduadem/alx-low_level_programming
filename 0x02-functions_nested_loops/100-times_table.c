496619a9b55c6c5cfe10a65072c502b54ab7e826
abduadem <abdulrehmanadem4@gmail.com>
2022-12-14 02:04:43 -0800

    Task 12 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,49 @@
 #include "main.h"
-#include <stdio.h>
-print_times_table(int n);
-
 /**
- * print_times_table - prints time table with provided number
- * @n: the provided value by the user
- * Return: Always 0 (Success)
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return: void
  */
 
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
+	int a = 0, rep, b;
 
-	if (n < 0 || n >= 15)
+	if (n < 0 || n > 15)
 		return;
-	for (row = 0; row <= n; row++)
+
+	while (a <= n)
 	{
-		for (column = 0; column <= n; column++)
+		for (b = 0; b <= n; b++)
 		{
-			product = (row * column);
-			if (column == 0)
-				_putchar('0' + product);
+			rep = a * b;
+			if  (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
 			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (product <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
 			}
 		}
 		_putchar('\n');
+		a++;
 	}
 }
