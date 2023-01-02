9b4a879d8ec822bc424536964725e98c7bfc7c53
abduadem <abdulrehmanadem4@gmail.com>
2023-01-02 08:58:15 -0800

    Task 8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
 * set_string - set value of pointer to a char
-* @s: variable of type pointer
-* @to: char
+* @s: The pointer
+* @to: The char
 */
 
 void set_string(char **s, char *to)
