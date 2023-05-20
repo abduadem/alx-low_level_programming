d214cf00e31e3eb775422f61d58e4410078d8254
abduadem <abdulrehmanadem4@gmail.com>
2023-05-19 18:16:40 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print the alphabet in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
