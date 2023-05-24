f8d8415bc5975fc6d81fd79804cb7cc5b35a74d5
abduadem <abdulrehmanadem4@gmail.com>
2023-05-23 17:01:30 -0700

    Mandatory task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Auth: Gbemi Jacob Adebayo
+ * Desc: Header file containing declarations for all functions
+ *       used in this project.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void putformat(int);
+
+#endif
