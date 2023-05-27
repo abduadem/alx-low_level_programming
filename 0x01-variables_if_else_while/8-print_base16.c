66e1b944b9614b7b99da22e0ae7f06ffcb67292d
abduadem <abdulrehmanadem4@gmail.com>
2023-05-26 18:30:58 -0700

    Mandatory task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
+ * main - Function to print the base16 values 0-9 and s a-f
  *
- * Return: Always 0 (succes)
+ * Return: Always 0.
  */
 
 int main()
