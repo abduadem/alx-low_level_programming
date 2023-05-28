ddb969e1f2f68c2a15c5a4ffe5b0600ddf646480
abduadem <abdulrehmanadem4@gmail.com>
2023-05-28 08:47:06 -0700

    Advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * Main-  prints all possible different combinations of two digitS printed in ascending order.
+ * two digits must be different
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int m, n; /* m=tens and n=ones */
+
+	for (m = '0'; m <= '8'; m++)
+	{
+		for (n = '0'; n <= '9'; n++)
+		{
+			if (!(m == n) && (m < n)) /* Eliminates repitition and tens < ones */
+			{
+				putchar(m);
+				putchar(n);
+
+				if (!(n == '9' && m == '8'))/*addes commma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
