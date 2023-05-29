bf77f1377561ceb64f10f740adc764f02843e30a
abduadem <abdulrehmanadem4@gmail.com>
2023-05-29 15:05:13 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
-* _strlen - a function to print the length of a string
+* _strlen - a function to returns the length of a string
 * @s: string to be supplied
 * Return: Always 0 (Success)
 */
 
 int _strlen(char *s)
 {
-	int i = 0;
+	int n = 0;
 
-	while (s[i] != '\0')
+	while (s[n] != '\0')
 	{
-		i++;
+		n++;
 	}
 
-	return (i);
+	return (n);
 }
