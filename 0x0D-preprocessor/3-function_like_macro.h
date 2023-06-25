325f1752ca3a5c74c268569233a9b518a0b054e9
abduadem <abdulrehmanadem4@gmail.com>
2023-06-25 02:59:02 -0700

    Mandatory Task

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/*
+ * Description: Header file that defines a function like macro ABS(x)
+ */
+
+#define ABS(x) ((x) > 0 ? (x) : ((x) * (-1)))
+
+#endif
